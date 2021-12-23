8b5ead549d629d0a58f14ea37c7a40360fa5b7e1
先規　勇樹 <yukisaki80@gmail.com>
2021-12-23 23:35:14 +0900

    JSON形式で取得するように修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,9 @@ class SearchController < ApplicationController
   def search
     postal_code = params[:postal_code]
     params = URI.encode_www_form({zipcode: postal_code})
+    uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    result["results"]
   end
 end
