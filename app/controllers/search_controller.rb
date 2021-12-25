2d23428fbffa718588cc8fac9f35b4ef5165d8f0
先規　勇樹 <yukisaki80@gmail.com>
2021-12-25 19:31:40 +0900

    cssを追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
       uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
       response = Net::HTTP.get_response(uri)
       result = JSON.parse(response.body)
-      if result['results']
+      if result["results"]
         @zipcode = result["results"][0]["zipcode"]
         @address1 = result["results"][0]["address1"]
         @address2 = result["results"][0]["address2"]
